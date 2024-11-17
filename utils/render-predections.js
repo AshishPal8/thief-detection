4031ff5c046fa7f992482a3c6d41e9a7d59d58f0
AshishPal8 <ashish81301669@gmail.com>
2024-11-18 00:05:46 +0530

    first commit

---
+++ utils/render-predections.js
@@ -0,0 +1,41 @@
+import { throttle } from "lodash";
+
+export const renderPredictions = (predictions, ctx) => {
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+  const font = "16px sans-serif";
+  ctx.font = font;
+  ctx.textBaseline = "top";
+
+  predictions.forEach((prediction) => {
+    const [x, y, width, height] = prediction["bbox"];
+
+    const isPerson = prediction.class === "person";
+
+    ctx.strokeStyle = isPerson ? "#FF0000" : "#00FFFF";
+    ctx.lineWidth = 4;
+    ctx.strokeRect(x, y, width, height);
+
+    // fill color
+    ctx.fillStyle = `rgba(225, 0, 0, ${isPerson ? 0.2 : 0})`;
+    ctx.fillRect(x, y, width, height);
+
+    //draw label background
+    ctx.fillStyle = isPerson ? "#FF0000" : "#00FFFF";
+    const textWidth = ctx.measureText(prediction.class).width;
+    const textHeight = parseInt(font, 10);
+    ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
+
+    ctx.fillStyle = "#000000";
+    ctx.fillText(prediction.class, x, y);
+
+    if (isPerson) {
+      playAudio();
+    }
+  });
+};
+
+const playAudio = throttle(() => {
+  const audio = new Audio("/alarm.mp3");
+  audio.play();
+}, 8000);
